363cb497aca626c56e00372fce9987d958c5df4e
Sai Nimmagadda <snimmagadda@rlsolutions.com>
2016-07-13 13:42:30 -0400

    adding basic boxes

--- src/main.c
+++ src/main.c
@@ -4,30 +4,40 @@
 #include <string.h>
 #include <panel.h>
 
+
+
 /* This is a basic terminal TUI for the Pulse Appliance */
+int row, col;
+
+/* initialization of functions */
+void getDimensions(WINDOW *win, int *row, int *col, int *middleRow, int *middleCol);
 
 int main(int argc, char *argv[]){
-    WINDOW *my_wins[2];
-    PANEL *my_panels[2];
-    int row, col;                               /* initialize the maximum row and column sizes */
+    WINDOW *my_wins[3];
+    PANEL *my_panels[3];
+    int middleRow, middleCol;                   /* initialize the middle row and column locations for later use */
     initscr();                                  /* initialize the ncurses window and standard screen */
     cbreak();                                   /* read in characters as they are typed */
     noecho();                                   /* do not display the users input on cursor */
-    getmaxyx(stdscr, row, col);
-
-    my_wins[0] = newwin(row*.5, col*.25, 1, 1);          /* make a window */
-    box(my_wins[0], 0, 0);                      /* put box around the window */
-
-    my_panels[0] = new_panel(my_wins[0]);       /* add to panel for organization */
-    update_panels();                            /* write to virtual screen in correct order */
-    doupdate();                                 /* update the physical screen with panels */
-
+    getDimensions(stdscr, &row, &col, &middleRow, &middleCol); 
+    my_wins[0] = newwin((row*.5)-5, (col*.5)-5,0,5);
+    my_wins[1] = newwin((row*.5)-5, (col*.5)-5,0,middleCol);
+    box(my_wins[0],0,0);
+    box(my_wins[1],0,0);
+    my_panels[0]=new_panel(my_wins[0]);
+    my_panels[1]=new_panel(my_wins[1]);
+    update_panels();
+    doupdate();
+   
     getch();
     endwin();
-
-
-
 
     return 0;
 }
 
+void getDimensions(WINDOW *win, int *row, int *col, int *middleRow, int *middleCol){
+   getmaxyx(win, *row, *col);
+   *middleRow = *row/2;
+   *middleCol= *col/2;
+
+}
