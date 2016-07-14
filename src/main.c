623d71f1432f5e9c61f8a0c359e6b7838529b603
Sai Nimmagadda <snimmagadda@rlsolutions.com>
2016-07-14 15:54:40 -0400

    Addition of while loop to keep UI displayed

--- src/main.c
+++ src/main.c
@@ -19,20 +19,23 @@ void displayNetworkWindow(WINDOW *win, char *iface, char *ipaddress, char *gatew
 int main(int argc, char *argv[]){
     WINDOW *my_wins[3];
     PANEL *my_panels[3];
-    int middleRow, middleCol;                   /* initialize the middle row and column locations for later use */
+    int middleRow, middleCol,ch;                /* initialize the middle row and column locations for later use */
     initscr();                                  /* initialize the ncurses window and standard screen */
     cbreak();                                   /* read in characters as they are typed */
     noecho();                                   /* do not display the users input on cursor */
+    keypad(stdscr, TRUE);                       /* enable keypad input */
     getDimensions(stdscr, &row, &col, &middleRow, &middleCol); 
-   
+    
+    /* read network information file and extrapolate useful information */
     FILE *fp;
     fp = fopen("/home/snimmagadda/Documents/TUI/lib/interfaces","r");
     char iface[255], ipaddress[255], gateway[255], netmask[255], dns[255];
     getNetworkInfo(fp, iface, ipaddress, netmask, gateway, dns);     
-
+    fclose(fp);
+    
     /* Initialize color pairs that will be used in TUI */
     start_color();
-    init_pair(1, COLOR_BLACK, COLOR_GREEN);
+    init_pair(1, COLOR_BLACK, COLOR_GREEN); 
     init_pair(2, COLOR_BLACK, COLOR_WHITE);
 
     /* Initialize the top left and right windows as well as bottom middle window*/
@@ -45,20 +48,18 @@ int main(int argc, char *argv[]){
     my_panels[1] = new_panel(my_wins[1]);
     my_panels[2] = new_panel(my_wins[2]);
     drawBorders(my_wins[2]);
-    mvwprintw(my_wins[0], 0,0, "%s", ipaddress);
-    mvwprintw(my_wins[0], 1,0,"%s", iface);
-    mvwprintw(my_wins[0], 2, 0,"%s", netmask);
-    mvwprintw(my_wins[0], 3, 0, "%s", gateway);
-    mvwprintw(my_wins[0], 4, 0, "%s", dns);
+    
     /* Add color to window backgrounds */
     wbkgd(my_wins[0], COLOR_PAIR(1));
     wbkgd(my_wins[1], COLOR_PAIR(2));
-    
+    mvwprintw(my_wins[0], 0,0, "%s", ipaddress);
     update_panels();
     doupdate();
-
-    fclose(fp);
-    getch();
+    
+    /* F1 is exite from TUI */
+    while((ch = getch()) != KEY_F(1))
+    {
+    }
     endwin();
     return 0;
 }
