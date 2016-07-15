ac3dab84f65c7f7a0756deda4c285a27d9d45a9c
Sai Nimmagadda <snimmagadda@rlsolutions.com>
2016-07-15 15:56:07 -0400

    minor formatting

--- src/main.c
+++ src/main.c
@@ -58,7 +58,7 @@ int main(){
     mvprintw(rowStart+3, (col -strlen("System Information"))/2,"System Information");       /* Print title of Window */
     mvprintw(rowStart+2, (col -strlen("pulse.rlpulse.net"))/2, "pulse.rlpulse.net");
     rowStart+=4;
-    my_wins[2] = newwin((row-rowStart)-3, col-8,rowStart, 5);
+    my_wins[2] = newwin((row-rowStart)-3, col-10,rowStart, 5);
     my_panels[0] = new_panel(my_wins[0]);
     my_panels[1] = new_panel(my_wins[1]);
     my_panels[2] = new_panel(my_wins[2]);
@@ -216,5 +216,5 @@ void printPulse(WINDOW *win, int starty, int startx)
     mvwprintw(win, starty, startx+36, "/");
     mvwprintw(win,starty,startx+37,"-");
 
-
+    wrefresh(win);
 }
