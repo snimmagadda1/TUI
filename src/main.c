9ffb949fe3c42334a3d746b4afd11f22c8c70463
Sai Nimmagadda <snimmagadda@rlsolutions.com>
2016-07-15 15:58:30 -0400

    Formatting pulse logo

--- src/main.c
+++ src/main.c
@@ -58,7 +58,7 @@ int main(){
     mvprintw(rowStart+3, (col -strlen("System Information"))/2,"System Information");       /* Print title of Window */
     mvprintw(rowStart+2, (col -strlen("pulse.rlpulse.net"))/2, "pulse.rlpulse.net");
     rowStart+=4;
-    my_wins[2] = newwin((row-rowStart)-3, col-10,rowStart, 5);
+    my_wins[2] = newwin((row-rowStart)-2, col-10,rowStart, 5);
     my_panels[0] = new_panel(my_wins[0]);
     my_panels[1] = new_panel(my_wins[1]);
     my_panels[2] = new_panel(my_wins[2]);
@@ -144,7 +144,8 @@ void displayBottomWindow(WINDOW *win, char totalDisk[255], char availDisk[255], 
     mvwprintw(win, 3, 23, "%s kB", availRAM);
     mvwprintw(win, 3, 34, "/");
     mvwprintw(win, 3, 36, "%s kB", totalRAM);
-    printPulse(win, 5,2);
+    
+    printPulse(win, 6,2);
     wrefresh(win);
 }
 
